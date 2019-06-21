1ecec83b2b590663533e2b1a75be907ecd9b5197
dpj99 <dpj99@outlook.com>
2019-06-21 18:33:44 +0800

    蓝图注册循环导入问题&&变量类型注释

--- info/__init__.py
+++ info/__init__.py
@@ -9,10 +9,13 @@ from redis import StrictRedis
 
 from config import config
 
-from info.modules.index import index_blu
+# from info.modules.index import index_blu
 
 # 初始化数据库
+#  在Flask很多扩展里面都可以先初始化扩展的对象，然后再去调用 init_app 方法去初始化
 db = SQLAlchemy()
+
+redis_store = None # type: StrictRedis
 
 
 def setup_log(config_name):
@@ -42,6 +45,7 @@ def create_app(config_name):
     db.init_app(app)
 
     # 初始化redis存储对象
+    global redis_store
     redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
     # 开启CSRF保护，只做服务器验证功能
     CSRFProtect(app)
@@ -50,6 +54,8 @@ def create_app(config_name):
     Session(app)
 
     # 注册蓝图
+    from info.modules.index import index_blu
     app.register_blueprint(index_blu)
+
 
     return app
--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,10 @@
+
+from info import redis_store
+
 from info.modules.index import index_blu
 
 
 @index_blu.route('/')
 def index():
+    redis_store.set("qqq", "dddddpjjjpppp")
     return 'index'
