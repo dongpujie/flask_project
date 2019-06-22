d58cc9a200c365d6a5a76ae3189851c8fd9cb73b
dpj99 <dpj99@outlook.com>
2019-06-22 15:42:49 +0800

    网站图标显示->重启浏览器后正常了

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -13,6 +13,6 @@ def index():
 
 # 在打开网页的时候，浏览器会默认去请求根路径+favicon.ico作网站标签的小图标
 # send_static_file 是 flask 去查找指定的静态文件所调用的方法
-# @index_blu.route('/favicon.ico')
-# def favicon():
-#     return current_app.send_static_file('news/favicon.ico')
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
