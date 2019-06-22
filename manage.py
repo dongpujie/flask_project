67a68f063e461ebce21d43fd36914b7556d714fd
dpj99 <dpj99@outlook.com>
2019-06-22 14:21:19 +0800

    *导入模型类->数据库迁移&数据导入

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 from flask_script import Manager
 
-from info import create_app, db
+from info import create_app, db, models
 
 # manage.py是程序启动的入口，只关心启动的相关参数以及内容，不关心具体该
 # 如果创建app或者相关业务逻辑
